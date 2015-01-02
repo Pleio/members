22520be1e091c6f89d022c5fe4acb6ff4a65e5d5
Bart Jeukendrup <bart@jeukendrup.nl>
2015-01-02 16:52:48 +0100

    initial commit

---
+++ languages/en.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Members English language file
+ */
+
+$english = array(
+	'members:label:newest' => 'Newest',
+	'members:label:popular' => 'Popular',
+	'members:label:online' => 'Online',
+	'members:search' => 'Search members',
+	'members:title:search' => 'Member search for %s',
+);
+
+add_translation('en', $english);
